906bbf23e6463d166c54801a4a04dafcd106a8de
NJ <45715403+janakanuwan@users.noreply.github.com>
2023-02-22 21:45:16 +0800

    enable websocket server

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 ﻿import modules.fitbit_web.web_api as fitbit_web_api
 import modules.utilities.time as time_utility
+import modules.websocket_server.socket_server as socket_server
 
 print(time_utility.get_date())
 
+flag_is_running = False
 
-fitbit_credential = fitbit_web_api.read_fitbit_credential()
-fitbit_token = fitbit_web_api.read_fitbit_token()
-if fitbit_token is None:
-    fitbit_token = fitbit_web_api.get_authorize_token(fitbit_credential)
-    fitbit_web_api.save_fitbit_token(fitbit_token)
+def start_app():
+    global flag_is_running
 
-fitbit_client = fitbit_web_api.get_auth_client(fitbit_credential, fitbit_token)
-today_string = time_utility.get_date("%Y-%m-%d")
+    if flag_is_running:
+        print('Another instance is running')
+        return
 
-fitbit_raw_data_distance = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_DISTANCE, fitbit_web_api.DETAIL_LEVEL_1MIN)
-print(f'\ndistance: {fitbit_raw_data_distance}')
+    flag_is_running = True
 
-fitbit_raw_data_steps = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_STEPS, fitbit_web_api.DETAIL_LEVEL_1MIN)
-print(f'\nsteps: {fitbit_raw_data_steps}')
+    fitbit_credential = fitbit_web_api.read_fitbit_credential()
+    fitbit_token = fitbit_web_api.read_fitbit_token()
+    if fitbit_token is None:
+        fitbit_token = fitbit_web_api.get_authorize_token(fitbit_credential)
+        fitbit_web_api.save_fitbit_token(fitbit_token)
 
-fitbit_raw_data_heart_rate = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_HEART_RATE, fitbit_web_api.DETAIL_LEVEL_1SEC)
-print(f'\nheart rate: {fitbit_raw_data_heart_rate}')
+    fitbit_client = fitbit_web_api.get_auth_client(fitbit_credential, fitbit_token)
+    today_string = time_utility.get_date("%Y-%m-%d")
 
-df = fitbit_web_api.get_data_frame(fitbit_raw_data_heart_rate, fitbit_web_api.DATA_TYPE_HEART_RATE)
-df_last_row = df.iloc[-1] # time, value
-last_row_val = df_last_row['value']
-print(last_row_val)
+    # fitbit_raw_data_distance = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_DISTANCE, fitbit_web_api.DETAIL_LEVEL_1MIN)
+    # print(f'\ndistance: {fitbit_raw_data_distance}')
+    # 
+    # fitbit_raw_data_steps = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_STEPS, fitbit_web_api.DETAIL_LEVEL_1MIN)
+    # print(f'\nsteps: {fitbit_raw_data_steps}')
+
+    socket_server.start_server_threaded()
+    current_millis = 0
+
+    while flag_is_running:
+        if time_utility.get_current_millis() - current_millis > 10*1000:
+
+            fitbit_raw_data_heart_rate = fitbit_web_api.get_json_data(fitbit_client, today_string, fitbit_web_api.DATA_TYPE_HEART_RATE, fitbit_web_api.DETAIL_LEVEL_1SEC)
+            # print(fitbit_raw_data_heart_rate)
+            df = fitbit_web_api.get_data_frame(fitbit_raw_data_heart_rate, fitbit_web_api.DATA_TYPE_HEART_RATE)
+            df_last_row = df.iloc[-1] # time, value
+            last_row_val = df_last_row['value']
+
+            socket_server.send_data(f'HR: {last_row_val}')
+
+            # utilities.send_get_request(f'http://127.0.0.1:5050/?HR: {last_row_val}=1')
+
+            current_millis = time_utility.get_current_millis()
+
+
+        time_utility.sleep_seconds(1)
+
+    socket_server.stop_server_threaded()
+
+
+start_app()
---
+++ modules/websocket_server/socket_server.py
@@ -0,0 +1,91 @@
+﻿import asyncio
+import websockets
+from queue import Queue
+import traceback
+import sys
+import threading
+
+SERVER_IP = ""
+SERVER_PORT = 8080
+
+CONNECTIONS = set()
+
+tx_queue = Queue()
+rx_queue = Queue()
+
+# references: https://websockets.readthedocs.io/en/stable/reference/server.html 
+async def ws_echo(websocket):
+    global tx_queue, rx_queue
+
+    CONNECTIONS.add(websocket)
+    try:
+        async for rx_data in websocket:
+            rx_queue.put_nowait(rx_data)
+            print(f'Received data: {rx_data}')
+
+            if not tx_queue.empty():
+                tx_data = tx_queue.get_nowait()
+                await websocket.send(tx_data)
+                print(f'Sent data: {tx_data}')
+                del tx_data
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    finally:
+        CONNECTIONS.remove(websocket)
+
+
+# Broadcast message to all websocket clients. 
+def broadcastmsg(msg):
+    websockets.broadcast(CONNECTIONS, msg)
+
+
+async def ws_main():
+    async with websockets.serve(ws_echo, SERVER_IP, SERVER_PORT):
+        await asyncio.Future()  # run forever
+
+
+async def main():
+    websocket_task = asyncio.create_task(ws_main())
+    await asyncio.gather(
+        websocket_task
+    )
+
+
+def start_server():
+    asyncio.run(main())
+
+def stop_server():
+    pass
+    
+    
+def send_data(data):
+    global tx_queue
+
+    print(f'tx_size: {tx_queue.qsize()}')
+    tx_queue.put_nowait(data)
+
+
+def receive_data():
+    global rx_queue
+
+    if rx_queue.empty():
+        return None
+
+    print(f'rx_size: {rx_queue.qsize()}')
+
+    return rx_queue.get_nowait()
+
+
+server_thread = None
+
+def start_server_threaded():
+    global server_thread
+    server_thread = threading.Thread(target=start_server, daemon=True)
+    server_thread.start()
+
+
+def stop_server_threaded():
+    global server_thread
+    stop_server()
+
+    server_thread.join(timeout=2)
