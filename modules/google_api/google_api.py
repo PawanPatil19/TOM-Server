2ae39566cb64b558298ad64ac7ca91103354eabf
NJ <45715403+janakanuwan@users.noreply.github.com>
2023-05-23 23:10:21 +0800

    enabel google map api

---
+++ modules/google_api/google_api.py
@@ -0,0 +1,22 @@
+
+import json
+
+
+GOOGLE_CREDENTIAL_FILE = 'config/google_credential.json'  # has the 'map_api_key', ...
+KEY_MAP_API = 'map_api_key'
+
+
+# return {"map_api_key": "YYY"}
+def read_google_credential():
+    print('Reading Google credentials')
+    with open(GOOGLE_CREDENTIAL_FILE, 'r') as f:
+        credential = json.load(f)
+    return credential
+
+
+def get_google_credential(key, credential = None):
+    _credential = credential
+    if _credential is None:
+        _credential = read_google_credential()
+
+    return _credential[key]
