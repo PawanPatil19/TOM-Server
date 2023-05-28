48f570a20019b858beb88ef38e1e36e11dc95d1c
NJ <45715403+janakanuwan@users.noreply.github.com>
2023-05-28 13:18:20 +0800

    clean VideoDetection.py

--- modules/yolov8/VideoDetection.py
+++ modules/yolov8/VideoDetection.py
@@ -5,89 +5,103 @@ import time
 import sys
 
 
-class  VideoDetection:
+class VideoDetection:
     def __init__(
-            self, 
-            video_path = 0, 
-            save_path = "yolo_video_output.avi",
-            inference = True,
-            model = "./modules/yolov8/weights/model.pt",
-            save = False):
+            self,
+            video_path=0,
+            inference=True,
+            confidence_level=0.7,
+            model="./modules/yolov8/weights/model.pt",
+            save=False,
+            save_path="yolo_video_output.avi",
+    ):
         self.videoPath = video_path
-        self.savePath = save_path
         self.inference = inference
+        self.confidenceLevel = confidence_level
         self.model = model
         self.useWebcam = False
         self.useStream = False
         self.captureInProgress = False
         self.save = save
-    
-    
+        self.savePath = save_path
+        self.capture = None
+
         print("VideoCapture::__init__()")
         print("OpenCV Version : %s" % (cv2.__version__))
         print("===============================================================")
         print("Initialising Video Capture with the following parameters: ")
         print("   - Video path       : " + str(self.videoPath))
         print("   - Inference?       : " + str(self.inference))
-    
+
     def __IsCaptureDev(self, videoPath):
-        try: 
+        try:
             return '/dev/video' in videoPath.lower()
         except (ValueError, AttributeError):
             return videoPath == 0
 
     def __IsRtsp(self, videoPath):
         try:
-            if 'rtsp:' in videoPath.lower() or '/api/holographic/stream' in videoPath.lower(): #or '0' in videoPath:
+            if 'rtsp:' in videoPath.lower() or '/api/holographic/stream' in videoPath.lower():  # or '0' in videoPath:
                 return True
         except (ValueError, AttributeError):
             return False
-        
-        
-    
+
     def __enter__(self):
-        self.setVideoSource(self.videoPath)
+        self.set_video_source(self.videoPath)
         return self
-    
-    def setVideoSource(self, newVideoPath):
-        self.videoPath = newVideoPath
-        self.useWebcam = self.__IsCaptureDev(newVideoPath)
-        self.useStream = self.__IsRtsp(newVideoPath)
-        
+
+    def set_video_source(self, new_video_path):
+        if self.captureInProgress:
+            self.captureInProgress = False
+
+        if self.capture:
+            self.capture.release()
+            self.capture = None
+
+        self.videoPath = new_video_path
+        self.useWebcam = self.__IsCaptureDev(new_video_path)
+        self.useStream = self.__IsRtsp(new_video_path)
+
         if self.useWebcam:
             print("   - Using webcam")
         elif self.useStream:
             print("   - Using stream")
         else:
             print("   - Using video file")
-            
+
+        print("Video Path: " + str(self.videoPath))
+
         self.capture = cv2.VideoCapture(self.videoPath)
-        self.captureInProgress = True
-        
-    
+        if self.capture.isOpened():
+            self.captureInProgress = True
+        else:
+            print("\nWARNING : Failed to Open Video Source\n")
+
     def start(self):
         while True:
             if self.captureInProgress:
                 self.__Run__()
-                
+
             if not self.captureInProgress:
                 time.sleep(0.1)
-            
 
     def __Run__(self):
         print("VideoCapture::__Run__()")
+
+        camera_fps = int(self.capture.get(cv2.CAP_PROP_FPS))
+        frame_width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        if camera_fps == 0:
+            print("Error : Could not get FPS")
+            return
+
         # to save the video
-        if self.save :
-            writer= cv2.VideoWriter(self.savePath, 
-                                    cv2.VideoWriter_fourcc(*'DIVX'), 
-                                    7, 
-                                    (1280, 720))
-        
-        print("Video Path: " + str(self.videoPath))
-        # define resolution
-        cap = cv2.VideoCapture(self.videoPath)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        if self.save:
+            writer = cv2.VideoWriter(self.savePath,
+                                     cv2.VideoWriter_fourcc(*'DIVX'),
+                                     camera_fps,
+                                     (frame_width, frame_height))
 
         # specify the model
         model = YOLO(self.model)
@@ -99,37 +113,42 @@ class  VideoDetection:
             text_scale=1
         )
 
-
         while True:
-            ret, frame = cap.read()
-            result = model(frame, agnostic_nms=True)[0]
+            ret, frame = self.capture.read()
+            #  iou=0.45, max_det=50, verbose=False
+            result = model(frame, agnostic_nms=True, conf=self.confidenceLevel)[0]
+            # [[bounding_boxes, mask, confidence, class_id, tracker_id]
             detections = sv.Detections.from_yolov8(result)
-            labels = [
-                f"{model.model.names[class_id]} {confidence:0.2f}"
-                for _, _, confidence, class_id, _
-                in detections
+
+            # [[class_label, confidence, bounding_boxes]]
+            detection_results = [
+                [model.model.names[class_id], confidence, bounding_boxes]
+                for bounding_boxes, _, confidence, class_id, _ in detections
             ]
+            labels = [f'{class_label} {confidence:0.2f}' for class_label, confidence, _ in
+                      detection_results]
+
             frame = box_annotator.annotate(
-                scene=frame, 
-                detections=detections, 
+                scene=frame,
+                detections=detections,
                 labels=labels
-            ) 
-            
+            )
+
             if self.save:
                 writer.write(frame)
-            
-            
-            cv2.imshow("yolov8", frame)
 
-            if (cv2.waitKey(30) == 27): # break with escape key
+            cv2.imshow("YOLOv8", frame)
+
+            if cv2.waitKey(30) == 27:  # break with escape key
                 break
-                
-        cap.release()
+
         if self.save:
             writer.release()
-        cv2.destroyAllWindows()
-        
-        
+
     def __exit__(self, exception_type, exception_value, traceback):
         # self.imageServer.close()
+
+        if self.capture:
+            self.capture.release()
+
         cv2.destroyAllWindows()
