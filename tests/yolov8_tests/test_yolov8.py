7e2d1c7ebaf645ab54c3c69c68d426e54cadd48b
NJ <45715403+janakanuwan@users.noreply.github.com>
2023-05-29 09:55:18 +0800

    add tests for yolov8

---
+++ tests/yolov8_tests/test_yolov8.py
@@ -0,0 +1,14 @@
+﻿# coding=utf-8
+
+import pytest
+import sys
+
+from modules.yolov8.VideoDetection import VideoDetection as YoloDetector
+
+def test_yolo():
+    try:
+        with YoloDetector(0) as yoloDetector:
+            yoloDetector.start()
+    except KeyboardInterrupt:
+        print("Yolo module stopped" )
+        
