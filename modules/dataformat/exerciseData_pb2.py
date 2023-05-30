d94e4c18a171d37e6e5ee272ce08bf5ba2bd43a4
Jarrett Teo <jarrett0203@gmail.com>
2023-05-30 10:15:47 +0800

    update exerciseData to include exerciseType

--- modules/dataformat/exerciseData_pb2.py
+++ modules/dataformat/exerciseData_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x65xerciseData.proto\"\x87\x02\n\x0c\x45xerciseData\x12\x12\n\nstart_time\x18\x01 \x01(\x03\x12\x13\n\x0bupdate_time\x18\x02 \x01(\x03\x12\x10\n\x08\x64uration\x18\x03 \x01(\x03\x12\x10\n\x08latitude\x18\x04 \x01(\x01\x12\x11\n\tlongitude\x18\x05 \x01(\x01\x12\x10\n\x08\x64istance\x18\x06 \x01(\x01\x12\x10\n\x08\x63\x61lories\x18\x07 \x01(\x01\x12\x12\n\nheart_rate\x18\x08 \x01(\x01\x12\x16\n\x0eheart_rate_avg\x18\t \x01(\x01\x12\r\n\x05steps\x18\n \x01(\x05\x12\r\n\x05speed\x18\x0b \x01(\x01\x12\x11\n\tspeed_avg\x18\x0c \x01(\x01\x12\x16\n\x0e\x63urrent_status\x18\r \x01(\tB(\n\x13\x63om.hci.tom.androidB\x11\x45xerciseDataProto')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x65xerciseData.proto\"\x9e\x02\n\x0c\x45xerciseData\x12\x12\n\nstart_time\x18\x01 \x01(\x03\x12\x13\n\x0bupdate_time\x18\x02 \x01(\x03\x12\x10\n\x08\x64uration\x18\x03 \x01(\x03\x12\x10\n\x08latitude\x18\x04 \x01(\x01\x12\x11\n\tlongitude\x18\x05 \x01(\x01\x12\x10\n\x08\x64istance\x18\x06 \x01(\x01\x12\x10\n\x08\x63\x61lories\x18\x07 \x01(\x01\x12\x12\n\nheart_rate\x18\x08 \x01(\x01\x12\x16\n\x0eheart_rate_avg\x18\t \x01(\x01\x12\r\n\x05steps\x18\n \x01(\x05\x12\r\n\x05speed\x18\x0b \x01(\x01\x12\x11\n\tspeed_avg\x18\x0c \x01(\x01\x12\x16\n\x0e\x63urrent_status\x18\r \x01(\t\x12\x15\n\rexercise_type\x18\x0e \x01(\tB(\n\x13\x63om.hci.tom.androidB\x11\x45xerciseDataProto')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -23,5 +23,5 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'\n\023com.hci.tom.androidB\021ExerciseDataProto'
   _globals['_EXERCISEDATA']._serialized_start=23
-  _globals['_EXERCISEDATA']._serialized_end=286
+  _globals['_EXERCISEDATA']._serialized_end=309
 # @@protoc_insertion_point(module_scope)
